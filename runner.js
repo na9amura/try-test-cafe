8f81beb9d04636cdea4f99ecbda9073919a19d0e
Takahiro Nakamura <dxbxdxbxdxb+dev@gmail.com>
2019-03-15 19:35:06 +0900

    test remote connection

--- runner.js
+++ runner.js
@@ -1,21 +1,28 @@
 require("dotenv").config();
 const createTestCafe = require("testcafe");
 let testcafe = null;
+let runner = null;
 
-createTestCafe("localhost", 1337, 1338)
+createTestCafe("127.0.0.1", 1337, 1338)
   .then(tc => {
     testcafe = tc;
-    const runner = testcafe.createRunner();
-    return runner
-      .src("./tests")
-      .browsers(["chrome"])
-      .run();
+    runner = testcafe.createRunner();
+    return testcafe.createBrowserConnection();
   })
-  .then(failedCount => {
-    console.log({ failedCount });
-    testcafe.close();
-  })
-  .catch(e => {
-    console.error(e);
-    testcafe.close();
+  .then(connection => {
+    console.log(connection.url);
+    return connection.once("ready", () => {
+      runner
+        .src("./tests")
+        .browsers(["chrome"])
+        .run()
+        .then(failedCount => {
+          console.log({ failedCount });
+          testcafe.close();
+        })
+        .catch(e => {
+          console.error(e);
+          testcafe.close();
+        });
+    });
   });
