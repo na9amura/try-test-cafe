8374c49f3baeb169b4eff73e011b2c84d7837c76
Takahiro Nakamura <dxbxdxbxdxb+dev@gmail.com>
2019-03-15 21:18:31 +0900

    down test-cafe version to 1.0.1 see: DevExpress/testcafe#3541

--- tests/sample-test.js
+++ tests/sample-test.js
@@ -5,6 +5,7 @@ fixture`Getting Started`.page`http://devexpress.github.io/testcafe/example`;
 test("My first test", async t => {
   await t
     .typeText("#developer-name", "John Smith")
+    .takeElementScreenshot("#developer-name")
     .click("#submit-button")
     .expect(Selector("#article-header").innerText)
     .eql("Thank you, John Smith!");
