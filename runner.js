ee6ae47253544be6d698794b301bfdd08bfd877f
Takahiro Nakamura <dxbxdxbxdxb+dev@gmail.com>
2019-03-15 18:36:51 +0900

    I could specify test file directory only directory path

--- runner.js
+++ runner.js
@@ -1,16 +1,13 @@
 require("dotenv").config();
 const createTestCafe = require("testcafe");
-const fs = require("fs");
 let testcafe = null;
 
 createTestCafe("localhost", 1337, 1338)
   .then(tc => {
-    const tests = fs.readdirSync("./tests").map(path => `./tests/${path}`);
-
     testcafe = tc;
     const runner = testcafe.createRunner();
     return runner
-      .src(tests)
+      .src("./tests")
       .browsers(["chrome"])
       .run();
   })
