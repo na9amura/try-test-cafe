1b89656269b716cd417b6f8db11a97c34f4711f4
Takahiro Nakamura <dxbxdxbxdxb+dev@gmail.com>
2019-03-15 18:04:53 +0900

    run tests with programming interface

--- runner.js
+++ runner.js
@@ -1,18 +1,21 @@
 require("dotenv").config();
 const createTestCafe = require("testcafe");
+const fs = require("fs");
 let testcafe = null;
 
 createTestCafe("localhost", 1337, 1338)
   .then(tc => {
+    const tests = fs.readdirSync("./tests").map(path => `./tests/${path}`);
+
     testcafe = tc;
     const runner = testcafe.createRunner();
     return runner
-      .src("local-test.js")
-      .browsers(["browserstack:Edge@15.0:Windows 10"])
+      .src(tests)
+      .browsers(["chrome"])
       .run();
   })
-  .then(res => {
-    console.log({ res });
+  .then(failedCount => {
+    console.log({ failedCount });
     testcafe.close();
   })
   .catch(e => {
